5a0e9800fdfb64df2e36e9ffedbb3a0794c68b94
lubang <lubang@lulab.net>
2018-03-24 21:34:08 +0900

    configure bootstrap 4.0 with dependencies
    - add jquery, pooper.js, boostrap
    - configure jquery.min js
    - configure bootstrap.min.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import '../node_modules/jquery/dist/jquery.min';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
